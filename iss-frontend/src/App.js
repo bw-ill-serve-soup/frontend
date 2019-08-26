0b42938e5765a884c0c351ca256feec3570ec8e1
masonkarsevar <masonkarsevar@gmail.com>
2019-08-26 11:57:01 -0700

    added react router dom and some routes to app.js component

--- iss-frontend/src/App.js
+++ iss-frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Route path='/add-form' />
+      <Route exact path='/' />
+      <Route path='/login' />
+      <Route path='/signup' />
     </div>
   );
 }
---
+++ iss-frontend/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div className='navigation-container'>
+
+        </div>
+    )
+}
+
+export default NavBar;
