2e2226f01d8066c982c7236ae8ade890f670e7ba
74ultra <r.barnhill@protonmail.com>
2019-08-26 14:16:50 -0700

    form component added (styling is only temporary). Added temporary Route and Link to App.js for testing. Added partially build axios call to service the form component.

--- iss-frontend/src/App.js
+++ iss-frontend/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import { axiosWithAuth } from './Auth/axiosWithAuth.js';
+
+import AddItemForm from './components/AddItemForm.js';
 
 function App() {
+  
+
+  // State and callbacks for the AddItemForm
+
+  
+
+  const addItem = (item) => {
+    console.log('you have added an inventory item', item);
+    axiosWithAuth().post('https://soupkitchen-buildweek.herokuapp.com/kitchen/inventory')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => console.log('There was an error', error))
+  };
+  
   return (
     <div className="App">
+
+      <Link to="/add_item">Add an item</Link>
+      <Route
+        exact
+        path="/add_item"
+        render={props => <AddItemForm {...props} addItem={addItem} />}
+      />
+
     </div>
   );
 }
