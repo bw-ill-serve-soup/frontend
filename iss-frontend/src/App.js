11737692e8cbfa6d719336d00b23d158cd34edda
masonkarsevar <masonkarsevar@gmail.com>
2019-08-26 12:20:48 -0700

    basic navigation coded will need to think of a way to work with semantic uis <a> tags

--- iss-frontend/src/App.js
+++ iss-frontend/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom';
 
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Route path='/add-form' />
       <Route exact path='/' />
       <Route path='/login' />
--- iss-frontend/src/components/NavBar.js
+++ iss-frontend/src/components/NavBar.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Menu, Segment} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
-function NavBar() {
-    return (
-        <div className='navigation-container'>
 
-        </div>
-    )
+class NavBar extends Component {
+    state = {activeItem: 'home'}
+
+    handleItemClick = (e, {name}) => {
+        this.setState({activeItem: name})
+    }
+
+    render() {
+        const {activeItem} = this.state
+        return (
+            <div className='navigation-container'>
+                <Menu pointing secondary>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem=== 'home'}
+                        onClick={this.handleItemClick}
+                    >
+                        <Link to='/'>Home</Link>
+                    </Menu.Item>
+                    <Menu.Item
+                        name='add inventory'
+                        active={activeItem === 'add inventory'}
+                        onClick={this.handleItemClick}
+                    >
+                        <Link to='/add-form'>Add Inventory</Link>
+                    </Menu.Item>
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                            name='login'
+                            active={activeItem === 'login'}
+                            onClick={this.handleItemClick}
+                        >
+                            <Link to='/login'>Login</Link>
+                        </Menu.Item>
+                        <Menu.Item
+                            name='signup'
+                            active={activeItem === 'signup'}
+                            onClick={this.handleItemClick} 
+                        >
+                            <Link to='/signup'>Signup</Link>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+            </div>
+        )
+    }
 }
 
 export default NavBar;
