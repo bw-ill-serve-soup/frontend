a36ffaff96717d7a2dbb1bc25b1c91c0cb3925bc
masonkarsevar <masonkarsevar@gmail.com>
2019-08-26 12:36:25 -0700

    route experiment. WIP: protected and unprotected routes

--- iss-frontend/src/App.js
+++ iss-frontend/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import {Route} from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import InventoryForm from './components/InventoryForm';
+import Login from './components/Login';
+import InventoryList from './components/InventoryList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Route path='/add-form' />
+      <Route path='/add-form'/>
       <Route exact path='/' />
-      <Route path='/login' />
+      <Route path='/login' component={Login} />
       <Route path='/signup' />
     </div>
   );
