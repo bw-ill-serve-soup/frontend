1480f8c3112baa278b5eb0b2a3c57cca5ea9dad4
masonkarsevar <masonkarsevar@gmail.com>
2019-08-28 11:58:55 -0700

    index.js converted

--- iss-frontend/src/index.js
+++ iss-frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
+import reducer from './reducers';
 
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
     document.getElementById('root'));
 
--- iss-frontend/src/reducers/index.js
+++ iss-frontend/src/reducers/index.js
@@ -1,1 +1,21 @@
-/*export {reducer } from './userReducer.js'*/
+const initialState = {
+    inventory: [],
+    error: ''
+}
+
+function reducer(state=initialState, action) {
+    console.log('reducer', action);
+
+    switch (action.type) {
+        case FETCH_INVENTORY_START:
+            console.log(action) 
+            return {
+                ...state
+            }
+        
+        default:
+            return {
+                ...state
+            }
+    }
+}
